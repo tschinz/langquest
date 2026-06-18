de20a0fe1a442758719b4e54d12fc7a1750a8dc8
axel.amand <axel.amand@hes-so.ch>
2026-06-18 09:22:55 +0200

    feat(riscv): output cycles not found if missing from ripes output

--- src/runner.rs
+++ src/runner.rs
@@ -740,6 +740,10 @@ fn verify_riscv(exercise: &Exercise, ripes_cfg: &RipesConfig) -> VerificationRes
   if let Some(c) = cycles {
     report.push(format!("Cycles: {c}"));
     report.push(String::new());
+  } else {
+    
+    report.push(format!("Cycles count not found in Ripes output."));
+    report.push(String::new());
   }
 
   for directive in &directives.expected_regs {
